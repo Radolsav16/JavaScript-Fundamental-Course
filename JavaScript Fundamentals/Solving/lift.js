036e8e02405024cdf713b55efb847604470c36ef
Radoslav <todorovradoslav6@gmail.com>
2024-08-13 18:48:47 +0300

    Make some logic for the exercise

--- JavaScript Fundamentals/Solving/lift.js
+++ JavaScript Fundamentals/Solving/lift.js
@@ -2,6 +2,12 @@ function solve(input){
     let people = Number(input.shift());
     let currLift = input.shift();
     let liftArray = currLift.split(" ").map(Number);
+
+
+    function findFreeSpace(array){
+        let istrue = array.find((a)=> a < 4 );
+        return istrue;
+    }
     
     for(let i = 0; i < liftArray.length;i++){
         let wagon = liftArray[i];
@@ -11,9 +17,19 @@ function solve(input){
             wagon++;
             liftArray[i] = wagon;
             people--;
+
             if(people <= 0){ 
                    isPeople = false;
                    break; 
+            }
+            if(i === liftArray.length - 1 && people > 0){
+                console.log(liftArray.split(" "));
+            }else if(liftArray[i] ===  4 && people > 0){
+                console.log(`There isn't enough space! ${people} people in a queue!`);
+                
+            }else if(findFreeSpace(liftArray) === true){
+                console.log(`The lift has empty spots!`);
+                
             }
         }
         if(!isPeople){
@@ -21,7 +37,7 @@ function solve(input){
         }
         
     }
-    console.log(liftArray);
+    
     
     
     
