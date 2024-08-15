300f2d352dcf4ac663156a57be1406d14e91d351
Radoslav <todorovradoslav6@gmail.com>
2024-08-15 23:19:40 +0300

    Make another exercise almost done

---
+++ JavaScript Fundamentals/Solving/bakeryShop.js
@@ -0,0 +1,55 @@
+function bakeryShop(input) {
+  let commands = input.shift();
+  const shop = {};
+  let soleFoodQuantitu = 0;
+
+  while (commands !== "Complete") {
+    let [cmd, quantity, food] = commands.split(" ");
+    quantity = Number(quantity);
+    if (cmd === "Receive") {
+      if (quantity <= 0) {
+        continue;
+      }
+
+      if (shop[food]) {
+        shop[food] += quantity;
+      } else {
+        shop[food] = quantity;
+      }
+    } else {
+      if (shop[food]) {
+        if (shop[food] < quantity) {
+          delete shop[food];
+          console.log(
+            `There aren't enough ${food}. You sold the last ${quantity} of them.`
+          );
+          soleFoodQuantitu += quantity;
+        } else {
+          shop[food] -= quantity;
+          console.log(`You sold ${quantity} ${food}.`);
+
+          if (shop[food] === 0) {
+            delete shop[food];
+            soleFoodQuantitu += quantity;
+          }
+        }
+      } else {
+        console.log(`You do not have any ${food}.`);
+      }
+    }
+
+    commands = input.shift();
+  }
+
+  for (let foods in shop) {
+    console.log(`${foods}: ${shop[foods]}`);
+  }
+  console.log(`All sold: ${soleFoodQuantitu} goods`);
+}
+bakeryShop([
+  "Receive 105 cookies",
+  "Receive 10 donuts",
+  "Sell 10 donuts",
+  "Sell 1 bread",
+  "Complete",
+]);
--- JavaScript Fundamentals/Solving/lift.js
+++ JavaScript Fundamentals/Solving/lift.js
@@ -1,57 +1,43 @@
-function solve(input){
-    let people = Number(input.shift());
-    let currLift = input.shift();
-    let liftArray = currLift.split(" ").map(Number);
+function solve(input) {
+  let people = Number(input.shift());
+  let currLift = input.shift();
+  let liftArray = currLift.split(" ").map(Number);
 
+  function lisftCheck(array) {
+    let filterArray = array.filter((a) => a < 4);
 
-    function lisftCheck(array){
-        let filterArray = array.filter((a)=> a < 4);
-
-        if(filterArray.length > 0){
-            return true;
-        }else{
-            return false;
-        }
+    if (filterArray.length > 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
+  for (let i = 0; i < liftArray.length; i++) {
+    let wagon = liftArray[i];
+    let isPeople = true;
 
-    
-    
-    for(let i = 0; i < liftArray.length;i++){
-        let wagon = liftArray[i];
-        let isPeople = true;
-        
-        while(wagon !== 4){
-            wagon++;
-            liftArray[i] = wagon;
-            people--;
+    while (wagon !== 4) {
+      wagon++;
+      liftArray[i] = wagon;
+      people--;
 
-
-            if(people <= 0 && lisftCheck(liftArray)){
-                console.log(`The lift has empty spots!`);
-                console.log(liftArray.join(" "));
-                isPeople = false;
-                break;
-            }else if(people > 0 && !lisftCheck(liftArray)){
-                console.log(`There isn't enough space! ${people} people in a queue!`);
-                console.log(liftArray.join(" "));
-                
-            }else if(!lisftCheck(liftArray) && people <= 0){
-                isPeople = false;
-                console.log(liftArray.split(" "));
-            }
-        }
-        if(!isPeople){
-            break;
-        }
-        
+      if (people <= 0 && lisftCheck(liftArray)) {
+        console.log(`The lift has empty spots!`);
+        console.log(liftArray.join(" "));
+        isPeople = false;
+        break;
+      } else if (people > 0 && !lisftCheck(liftArray)) {
+        console.log(`There isn't enough space! ${people} people in a queue!`);
+        console.log(liftArray.join(" "));
+      } else if (!lisftCheck(liftArray) && people <= 0) {
+        isPeople = false;
+        console.log(liftArray.split(" "));
+      }
     }
-    
-    
-    
-    
+    if (!isPeople) {
+      break;
+    }
+  }
 }
-solve([
-    "15",
-    "0 0 0 0 0"
-   ]);
+solve(["20", "0 2 0"]);
