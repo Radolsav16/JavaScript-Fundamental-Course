b0745090b8ee178396b6cbf2f808689301f1a6de
Radoslav <todorovradoslav6@gmail.com>
2024-08-21 16:47:36 +0300

    Make catalog page

--- JavaScript Fundamentals/Exprees-Demo/index.js
+++ JavaScript Fundamentals/Exprees-Demo/index.js
@@ -1,9 +1,12 @@
+
 const express = require('express');
 
 const app = express();
 
 app.get('/catalog',(req,res)=>{
 
-    res.write("Zdrasti")
+    res.write(`<h1>This is Catalog Page</h1>`)
     res.end();
 }).listen(3400,()=>console.log('Surver is loading on 3400'));
+
+
